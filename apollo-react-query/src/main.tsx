97c4fe3b6311dd0e53949b8b04dd51109b901b9c
Irfanizam <irfanizam34@gmail.com>
2022-09-25 21:19:48 +0800

    country-test

--- apollo-react-query/src/main.tsx
+++ apollo-react-query/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const queryClient = new QueryClient();
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <QueryClientProvider client={queryClient} >
+      <App/>
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
